975a2934b637227df22bf698452fd32b5c5794f6
Richard <yangrich91@gmail.com>
2020-09-23 01:15:31 -0400

    test

--- models/users.js
+++ models/users.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  role: String,
+  // role: String,
 });
 
 const User = mongoose.model("User", userSchema);
